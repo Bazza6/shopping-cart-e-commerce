3146694f140e289e20bf4e53b301df992ed8bbbc
Bazza <nazareno.valentini@gmail.com>
2022-06-28 10:06:56 +0200

    remove item button and some style

--- js/shop.js
+++ js/shop.js
@@ -1,5 +1,5 @@
 "use strict";
-let numeroCarro = 0;
+let numeroCarro = 0;  //el numero que sale en el boton del carrito
 
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
 var products = [
@@ -85,7 +85,6 @@ function buy(id) {
             console.clear()
             console.log("cartList:");
             console.table(cartList);
-            // i = productLength;
         }
     }
     numeroCarro++;
@@ -97,7 +96,7 @@ function cleanCart() {
     cartList.length = 0;
     console.log("cartList:", cartList);
 
-    // esto para vaciar el carrito en el cartModal. REVISAR!!!!
+    // esto para vaciar el carrito en el cartModal. 
     cart.length = 0;
     generateCart();
     printCart();
@@ -136,7 +135,6 @@ function generateCart() {
     console.log("cart:")
     console.table(cart);
     applyPromotionsCart(); // Aplicamos las promociones!!!
-
 }
 
 // Exercise 5
@@ -144,7 +142,6 @@ function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
     let cartLength = cart.length;
     for (let i = 0; i < cartLength; i++) {
-        // let indexID = cartList[i].id
         cart[i].subtotal = (cart[i].price) * (cart[i].quantity);
         cart[i].subtotalWithDiscount = (cart[i].price) * (cart[i].quantity); //pongo con descuento tambien si no hay para facilitar la insercion de datos en el Ex.6
         if ((cart[i].id == 1) && (cart[i].quantity >= 3)) {  // oferta cooking oil
@@ -158,13 +155,20 @@ function applyPromotionsCart() {
     console.table(cart);
 }
 
+function emptyCart() { //para vaciar el carrito
+    document.getElementById("cart_list").innerHTML = "";
+    document.getElementById("total_price").innerHTML = 0;
+}
+
 // Exercise 6
+// Fill the shopping cart modal manipulating the shopping cart dom
 function printCart() {
-    // Fill the shopping cart modal manipulating the shopping cart dom
+    emptyCart();
+    applyPromotionsCart();
     for (let i = 0; i < cart.length; i++) {
-        document.getElementById("cart_list").innerHTML += "<tr><th>" +
-            cart[i].name + "</th><td>" + cart[i].price + "</td><td>" +
-            cart[i].quantity + "</td><td>$" + cart[i].subtotalWithDiscount + "</td>";
+        document.getElementById("cart_list").innerHTML += `<tr><th>${cart[i].name} </th><td> ${cart[i].price} </td><td>
+            ${cart[i].quantity} </td><td><button type="button" class="btn-small btn-circle btn-outline-danger" 
+            onclick="removeFromCart(${cart[i].id})">-</button></td><td>$ ${cart[i].subtotalWithDiscount}</td>`; //agregamos el boton para borrar los items
     }
     let totalPrice = 0;
     for (let i = 0; i < cart.length; i++) {
@@ -182,15 +186,9 @@ function addToCart(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cart array or update its quantity in case it has been added previously.
 
-
     let productLength = products.length;
-
-
     for (let i = 0; i < productLength; i++) {
-        //let indexID = cart[i].id;   //el ID de cada item
-
         let indexCart = cart.findIndex(e => e.id == products[i].id) //la posicion en cart del item que tiene ese ID
-
         if (products[i].id == id) {
             if (indexCart < 0) {    //si en cart no hay item con ese ID lo añado y le pondo quantity = 1
                 cart.push(products[i]);
@@ -200,31 +198,17 @@ function addToCart(id) {
             }
         }
     }
+    numeroCarro++;
+    document.getElementById("count_product").innerHTML = numeroCarro; //numero carrito
     console.clear();
     console.table(cart);
-
 }
-
-
-
-/*     for (let i = 0; i < cartLength; i++) {
-        let indexID = cartList[i].id;   //el ID de cada item
-        let indexCart = cart.findIndex(e => e.id == indexID);   //la posicion en cart del item que tiene ese ID
-        if (indexCart < 0) {    //si en cart no hay item con ese ID lo añado y le pondo quantity = 1
-            cart.push(cartList[i]);
-            cart.at(-1).quantity = 1;
-        } else {    //si en cart ya existe ese item le sumo 1 quantity
-            cart[indexCart].quantity++;
-        }
-    } */
-
 
 // Exercise 9
 function removeFromCart(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array ???
     let cartLength = cart.length;
-    console.log("id: ", id);
     for (let i = 0; i < cartLength; i++) {
         if ((id == cart[i].id) && (cart[i].quantity > 1)) {
             cart[i].quantity--;
@@ -233,6 +217,10 @@ function removeFromCart(id) {
             i = cartLength; // eso para parar el bucle FOR. Si no no encuentra el siguiente i (el array es mas corto)
         }
     }
+    numeroCarro--;
+    document.getElementById("count_product").innerHTML = numeroCarro;
+    printCart();
+    console.clear()
     console.table(cart);
 }
 
